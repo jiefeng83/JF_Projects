255f7eabb686ac4fe1498bc4d400adb5be022e86
jiefeng83 <koh_jiefeng@live.com>
2014-11-04 08:56:38 +0800

    add shirtek app2

---
+++ ShirtekApp2/ShirtekApp2/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace ShirtekApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.readButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column6,
+            this.Column9,
+            this.Column1,
+            this.Column10,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column7,
+            this.Column8});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 178);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "No";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 30;
+            // 
+            // Column9
+            // 
+            this.Column9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column9.HeaderText = "Inv No";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Date";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column10
+            // 
+            this.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column10.HeaderText = "Acct No";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 5;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.FillWeight = 60F;
+            this.Column2.HeaderText = "Store Code1";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.FillWeight = 60F;
+            this.Column3.HeaderText = "Store Code2";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.FillWeight = 300F;
+            this.Column4.HeaderText = "WO Number";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "Net Amount";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column11
+            // 
+            this.Column11.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column11.HeaderText = "Tax Amt";
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            this.Column11.Width = 5;
+            // 
+            // Column12
+            // 
+            this.Column12.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column12.HeaderText = "Total Amt";
+            this.Column12.Name = "Column12";
+            this.Column12.ReadOnly = true;
+            this.Column12.Width = 5;
+            // 
+            // Column13
+            // 
+            this.Column13.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column13.HeaderText = "Column13";
+            this.Column13.Name = "Column13";
+            this.Column13.ReadOnly = true;
+            this.Column13.Width = 5;
+            // 
+            // Column14
+            // 
+            this.Column14.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column14.HeaderText = "Column14";
+            this.Column14.Name = "Column14";
+            this.Column14.ReadOnly = true;
+            this.Column14.Width = 5;
+            // 
+            // Column15
+            // 
+            this.Column15.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column15.HeaderText = "Column15";
+            this.Column15.Name = "Column15";
+            this.Column15.ReadOnly = true;
+            this.Column15.Width = 5;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "DO Number";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column8.FillWeight = 200F;
+            this.Column8.HeaderText = "File Name";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseButton.Location = new System.Drawing.Point(591, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "browse...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathTextBox.Location = new System.Drawing.Point(57, 12);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(528, 20);
+            this.pathTextBox.TabIndex = 2;
+            this.pathTextBox.Text = "C:\\Users\\Jiefeng\\Desktop\\Shirtek\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder:";
+            // 
+            // readButton
+            // 
+            this.readButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.readButton.BackColor = System.Drawing.Color.LawnGreen;
+            this.readButton.Location = new System.Drawing.Point(672, 12);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 1;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = false;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusStrip1.Location = new System.Drawing.Point(2, 226);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.ReadOnly = true;
+            this.statusStrip1.Size = new System.Drawing.Size(756, 20);
+            this.statusStrip1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 250);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ShirtekApp";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.TextBox statusStrip1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+    }
+}
+
---
+++ ShirtekApp2/ShirtekApp2/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Threading;
+
+namespace ShirtekApp2
+{
+    public partial class Form1 : Form
+    {
+        List<string> fileList = new List<string>();
+        List<WorkOrderData> woDataList = new List<WorkOrderData>();
+
+        string fileName = @"C:\Users\Jiefeng\Desktop\data\McD 10 OCT  WO 2001x.xls";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            statusStrip1.Text = "Select folder and press the Read button.";
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pathTextBox.Text = folderBrowserDialog1.SelectedPath + @"\";
+            }
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            fileList.Clear();
+            woDataList.Clear();
+            dataGridView1.Rows.Clear();
+
+            if (!Directory.Exists(@pathTextBox.Text))
+            {
+                statusStrip1.Text = "Directory not found.";
+                return;
+            }
+
+            foreach (string s in Directory.GetFiles(@pathTextBox.Text, "*.xls").Select(Path.GetFileName))
+            {
+                fileList.Add(s);
+            }
+
+            int count = 0;
+            foreach (string fileName in fileList)
+            {
+                count++;
+                statusStrip1.Text = "Reading " + count + "/" + fileList.Count;
+
+                string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source='" + @pathTextBox.Text + fileName + "';" + "Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1;\"";
+                //var connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", fileName, "Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1;\"");
+
+                var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
+                var ds = new DataSet();
+
+                adapter.Fill(ds, "Sheet1");
+
+                DataTable dataTable = ds.Tables["Sheet1"];
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    if (dataTable.Rows[i][3] != null)
+                    {
+                        WorkOrderData woData = new WorkOrderData();
+
+                        woData.date = (dataTable.Rows[i][2] != null) ? dataTable.Rows[i][2].ToString() : "";
+                        woData.storeCode1 = (dataTable.Rows[i][4] != null) ? dataTable.Rows[i][4].ToString() : "";
+                        woData.storeCode2 = (dataTable.Rows[i][5] != null) ? dataTable.Rows[i][5].ToString() : "";
+                        woData.doNumber = (dataTable.Rows[i][13] != null) ? dataTable.Rows[i][13].ToString() : "";
+                        woData.woNumber = (dataTable.Rows[i][6] != null) ? dataTable.Rows[i][6].ToString() : "";
+                        woData.netAmount = (dataTable.Rows[i][7] != null) ? dataTable.Rows[i][7].ToString() : "";
+                        woData.invoiceDate =(dataTable.Rows[4][0] != null) ? dataTable.Rows[4][0].ToString() : "";
+                        woData.invoiceNo = (dataTable.Rows[i][1] != null) ? dataTable.Rows[i][1].ToString() : "";
+                        woData.fileName = fileName.Replace(".xls", "");
+
+                        woDataList.Add(woData);
+                    }
+                }
+            }
+
+            List<WorkOrderData> woDataListSorted = woDataList.OrderBy(o => o.fileName).ToList();
+
+            dataGridView1.SuspendLayout();
+            dataGridView1.Rows.Add(woDataList.Count);
+
+            int rowIndex = 0;
+            foreach (WorkOrderData wod in woDataListSorted)
+            {
+                dataGridView1.Rows[rowIndex].Cells[0].Value = rowIndex + 1;
+                dataGridView1.Rows[rowIndex].Cells[1].Value = "";
+                dataGridView1.Rows[rowIndex].Cells[2].Value = wod.date;
+                dataGridView1.Rows[rowIndex].Cells[3].Value = "7415-044";
+                dataGridView1.Rows[rowIndex].Cells[4].Value = wod.storeCode1;
+                dataGridView1.Rows[rowIndex].Cells[5].Value = wod.storeCode2;
+                dataGridView1.Rows[rowIndex].Cells[6].Value = wod.woNumber;
+                dataGridView1.Rows[rowIndex].Cells[7].Value = wod.netAmount;
+                dataGridView1.Rows[rowIndex].Cells[8].Value = "$0.00";
+                dataGridView1.Rows[rowIndex].Cells[9].Value = wod.netAmount;
+                dataGridView1.Rows[rowIndex].Cells[10].Value = "0%";
+                dataGridView1.Rows[rowIndex].Cells[11].Value = "";
+                dataGridView1.Rows[rowIndex].Cells[12].Value = "";
+                dataGridView1.Rows[rowIndex].Cells[13].Value = wod.doNumber;
+                dataGridView1.Rows[rowIndex].Cells[14].Value = wod.fileName;
+                rowIndex++;
+            }
+
+            //dataGridView1.Sort(dataGridView1.Columns[14], ListSortDirection.Ascending);
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+
+            dataGridView1.ResumeLayout();
+            dataGridView1.ClearSelection();
+            for (int i = 0; i < dataGridView1.ColumnCount - 1; i++)
+            {
+                for (int r = 0; r < dataGridView1.RowCount; r++)
+                    dataGridView1[i, r].Selected = true;
+            }
+            //dataGridView1.SelectAll();
+                        
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            Clipboard.SetDataObject(dataObj, true);
+
+            statusStrip1.Text = woDataListSorted.Count + " files read. Data copied to clipboard. Paste directly to Excel ;)";
+        }
+
+        private void testing(DataTable dataTable)
+        {
+            int x = 0, y = 0;
+            string temp = "";
+            for (int i = 0; i < 56; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    if (dataTable.Rows[i][j] != null)
+                    {
+                        temp = dataTable.Rows[i][j].ToString();
+
+                        if (temp.Contains("12345"))
+                        {
+                            x = i; y = j;
+                            statusStrip1.Text = x + "   " + y;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class WorkOrderData
+    {
+        public string date;
+        public string storeCode1;
+        public string storeCode2;
+        public string netAmount;
+        public string doNumber;
+        public string woNumber;
+        public string fileName;
+        public string invoiceDate;
+        public string invoiceNo; 
+    }
+}
